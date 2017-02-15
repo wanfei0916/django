0a7240e6808fd4f5acbe27ffa454d89df763a257
wanfei <wanfei.0916@163.com>
2017-02-15 13:49:41 +0800

    add paginator and search

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^tag/(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
-    url(r'^classify/$', views.classify, name='classify')
+    url(r'^classify/$', views.classify, name='classify'),
+    url(r'^search/$', views.blog_search, name='search'),
 ]
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from article.models import Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 '# Create your views here.'
 
 
 def home(request):
-    post_list = Article.objects.all()
+    posts = Article.objects.all()
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        post_list = paginator.page(1)
+    except EmptyPage:
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request, 'article/home.html', {'post_list': post_list})
 
 
@@ -34,3 +43,16 @@ def search_tag(request, tag):
         raise Http404
     return render(request, 'article/tag.html', {'post_list': post_list})
 
+
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request, 'article/home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains=s)
+            if len(post_list) == 0:
+                return render(request, 'article/classify.html', {'post_list': post_list, 'error': True})
+            else:
+                return render(request, 'article/classify.html', {'post_list': post_list, 'error': False})
+    return redirect('/')
